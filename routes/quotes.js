f446543794e12dd23cc3a33218059e42c82b92aa
Alanna Tai <alanna.tai@hotmail.com>
2019-05-24 18:12:22 -0400

    Add data to quote route

--- routes/quotes.js
+++ routes/quotes.js
@@ -1,5 +1,6 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+
 
 router.get('/', function (req, res, next) {
   let quotes = [
