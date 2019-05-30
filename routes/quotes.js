f128b86236cf760190a1bc5633be6d34a467fa89
Alanna Tai <alanna.tai@hotmail.com>
2019-05-30 18:43:31 -0400

    Rename name to author

--- routes/quotes.js
+++ routes/quotes.js
@@ -6,23 +6,23 @@ router.get('/', function (req, res, next) {
   let quotes = [
     {
       quote: '"I\'m not the strongest. I\'m not the fastest. But I\'m really good at suffering."',
-      name: 'Amelia Boone'
+      author: 'Amelia Boone'
     },
     {
       quote: '"It\'s unfortunate that this has happened. No. It\'s fortunate that this has happened and I\'ve remained unharmed."',
-      name: 'Marcus Aurelius'
+      author: 'Marcus Aurelius'
     },
     {
       quote: '"...there\'s only one really good question, which is, \'What am I unwilling to feel?\'"',
-      name: 'Tara Brach'
+      author: 'Tara Brach'
     },
     {
       quote: '"You must want to be a butterfly so badly, you are willing to give up being a caterpillar."',
-      name: 'Sekou Andrews'
+      author: 'Sekou Andrews'
     },
     {
       quote: '"Learn the rules like a pro, so you can break them like an artist."',
-      name: 'Pablo Picasso'
+      author: 'Pablo Picasso'
     }
   ];
 
